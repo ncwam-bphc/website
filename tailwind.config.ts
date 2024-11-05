e8d454641ac573d00ccce23d10dd47a73980d498
achaldps@gmail.com <achaldps@gmail.com>
2024-11-05 18:22:53 +0530

    feat: changed the heading colour to poppy

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ export default {
   			sm: 'calc(var(--radius) - 4px)'
   		},
   		colors: {
+			poppy: '#da583c',
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
