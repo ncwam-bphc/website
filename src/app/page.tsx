1ca53ce19c4ff5f6ece81f12ab2e57787e0c5644
Shiv Patil <krymzin0311@gmail.com>
2024-11-08 15:46:38 +0530

    fix: hydration error, legacy prop warning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,44 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react'
-import Image from 'next/image'
-import { Button } from "~/components/ui/button"
-import { MoveRight } from "lucide-react"
+import dynamic from "next/dynamic";
+import Image from "next/image";
+const Timer = dynamic(() => import("~/components/timer"), {
+  ssr: false,
+});
+import { Button } from "~/components/ui/button";
+import landingPageBg from "~/assets/landing.jpeg";
 
 export default function ConferenceLanding() {
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft())
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(calculateTimeLeft())
-    }, 1000)
-
-    return () => clearTimeout(timer)
-  })
-
-  function calculateTimeLeft() {
-    const difference = +new Date('2025-02-28') - +new Date()
-    let timeLeft = {}
-
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      }
-    }
-
-    return timeLeft
-  }
-
   return (
-    <div className="relative min-h-screen flex items-center justify-center overflow-hidden font-sans">
+    <div className="relative flex min-h-screen items-center justify-center overflow-hidden font-sans">
       <Image
-        src="/landing.jpeg"
+        src={landingPageBg}
         alt="Conference background"
-        layout="fill"
+        fill={true}
         objectFit="cover"
         className="z-0"
         priority
       />
-      <div className="absolute inset-0 bg-black bg-opacity-60 z-10"></div>
-      <div className="relative z-20 text-center text-white px-4 max-w-4xl mx-auto">
-        <h1 className="text-6xl md:text-8xl lg:text-9xl font-extrabold mb-6 tracking-tight">
+      <div className="absolute inset-0 z-10 bg-black bg-opacity-60"></div>
+      <div className="relative z-20 mx-auto max-w-4xl px-4 text-center text-white">
+        <h1 className="mb-6 text-6xl font-extrabold tracking-tight md:text-8xl lg:text-9xl">
           NCWAM <span className="text-[#da583c]"> 2025</span>
         </h1>
-        <p className="text-xl md:text-2xl mb-2 font-light">National Workshop on Challenges in Welding and Additive Manufacturing</p>
-        <p className="text-lg md:text-xl mb-2 font-medium">February 28, 2025</p>
-        <p className="text-lg md:text-xl mb-12 font-bold">BITS Pilani, Hyderabad Campus</p>
-
-        <div className="flex justify-center space-x-8 mb-12">
-          {Object.entries(timeLeft).map(([unit, value]) => (
-            <div key={unit} className="flex flex-col items-center">
-              <span className="text-4xl md:text-5xl font-bold mb-2 transition-all duration-300 ease-in-out transform hover:scale-110">{value as number}</span>
-              <span className="text-sm md:text-base uppercase tracking-wide">{unit}</span>
-            </div>
-          ))}
+        <p className="mb-2 text-xl font-light md:text-2xl">
+          National Workshop on Challenges in Welding and Additive Manufacturing
+        </p>
+        <p className="mb-2 text-lg font-medium md:text-xl">February 28, 2025</p>
+        <p className="mb-12 text-lg font-bold md:text-xl">
+          BITS Pilani, Hyderabad Campus
+        </p>
+        <div className="min-h-24">
+          <Timer />
         </div>
 
-        <Button size="lg" variant='poppy'>
+        <Button size="lg" variant="poppy">
           Register
         </Button>
       </div>
     </div>
-  )
+  );
 }
---
+++ src/components/timer.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+function calculateTimeLeft() {
+  const difference = new Date("2025-02-28").getTime() - Date.now();
+  if (difference > 0) {
+    return {
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / 1000 / 60) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    };
+  }
+}
+
+const Timer = () => {
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+  return (
+    <div className="mb-12 flex justify-center space-x-8">
+      {timeLeft &&
+        Object.entries(timeLeft).map(([unit, value]) => (
+          <div key={unit} className="flex flex-col items-center">
+            <span className="mb-2 transform text-4xl font-bold transition-all duration-300 ease-in-out hover:scale-110 md:text-5xl">
+              {value}
+            </span>
+            <span className="text-sm uppercase tracking-wide md:text-base">
+              {unit}
+            </span>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Timer;
