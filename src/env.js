d73de4c9539e77cf0e1ec2afa45f0cc124141987
achaldps@gmail.com <achaldps@gmail.com>
2024-11-10 18:56:17 +0530

    fix: added postgress url check

--- src/env.js
+++ src/env.js
@@ -7,7 +7,7 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.string().url().default("postgresql://postgres:password@localhost:5432/ncwam-website"),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
