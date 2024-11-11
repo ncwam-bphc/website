2efa02d3a7384b52d7d820c464711a228d404779
Max-034 <148268311+Max-034@users.noreply.github.com>
2024-11-12 00:27:17 +0530

    feat:speakers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ export const metadata: Metadata = {
   title: "NCWAM 2025",
   description: "NCWAM website",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
+
 };
 
 export default function RootLayout({
@@ -14,6 +15,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={GeistSans.variable}>
+    
       <body className="dark relative min-h-screen">
         <header className="navbar absolute left-0 right-0 top-0 z-50">
           <Navbar />
--- src/app/speakers/page.tsx
+++ src/app/speakers/page.tsx
@@ -1,9 +1,57 @@
 "use client";
+import { StaticImageData } from "next/image";
+import bits from "~/assets/about.jpeg";
+import Image from "next/image";
+import spbg from "~/assets/spbg2.jpeg";
+
+
+function Speak({img1 , img2 , img3}: {img1: StaticImageData , img2: StaticImageData , img3: StaticImageData}) {
+  return(
+    <div className="flex-col mt-10 gap-56 justify-center sm:flex sm:flex-row sm:mt-10 sm:gap-56 sm:justify-center">
+    <div className="flex flex-col ml-2 items-center">
+    <Image style = {{borderRadius: '100%'}} className="size-40" src= {img1} alt="img1" />
+  <div className="mt-8">NAME</div> <div>INSTITUTION</div>
+    </div>
+    <div className="flex flex-col items-center">
+    <Image style = {{borderRadius: '100%'}} className="size-40" src= {img2} alt="img1" />
+    <div className="mt-8">NAME</div> <div>INSTITUTION</div>
+  
+    </div>
+    <div className="flex flex-col mr-2 items-center">
+    <Image style = {{borderRadius: '100%'}} className="size-40" src= {img3} alt="img1" />
+   <div className="mt-8">NAME</div> <div>INSTITUTION</div>
+    </div>
+ 
+    </div>
+  )
+}
+
 
 export default function SpeakersPage() {
   return (
-    <main className="relative flex min-h-screen items-center justify-center overflow-hidden font-sans">
-      Speakers
+
+     <div className="content h-full gap-8 p-8 lg:p-16">
+        <Image
+          src={spbg}
+          alt="Conference background"
+          fill={true}
+          objectFit="cover"
+          className="-z-20 opacity-15"
+          priority
+        />
+      
+
+    <main className="relative flex-row min-h-screen overflow-hidden font-sans gap-40">
+      <div className="flex justify-center mt-40 mb-6 text-6xl font-bold text-[#da583c]">
+       MEET OUR
+      </div>
+      <div className="flex justify-center text-8xl border-b-4 border-white font-bold pb-8">SPEAKERS</div>
+      <Speak img1 = {bits} img2 = {bits} img3 = {bits}></Speak>
+      <Speak img1 = {bits} img2 = {bits} img3 = {bits}></Speak>
+      <Speak img1 = {bits} img2 = {bits} img3 = {bits}></Speak>
+    
     </main>
+
+    </div>
   );
 }
