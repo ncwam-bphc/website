68a92458a05d8a499f6ce12e4db1770fbe3336e1
Shiv Patil <krymzin0311@gmail.com>
2024-11-24 19:05:18 +0530

    feat: downloads page

--- src/app/downloads/page.tsx
+++ src/app/downloads/page.tsx
@@ -1,58 +1,104 @@
 "use client";
-import "~/styles/fullpagescroll.css";
-import Section from "~/components/FullpageSection";
-import bg from "~/assets/downloads/bg.webp";
 import { buttonVariants } from "~/components/ui/button";
 import { cn } from "~/lib/utils";
-import frontpage from "~/assets/downloads/frontpage.png";
-import backpage from "~/assets/downloads/backpage.png";
 
 export default function AboutusPage() {
   return (
-    <div className="container min-w-[320px]">
-      <main className="main">
-        <Section bgImage={bg} className="flex-col gap-1 md:gap-4">
-          <div className="flex items-center md:flex-col">
-            <div className="flex flex-col items-center font-bold">
-              <div className="text-nowrap text-center text-3xl uppercase text-accent md:text-3xl lg:text-5xl">
-                Downloads
-              </div>
-            </div>
+    <main className="flex flex-col items-center pt-40">
+      <div className="flex items-center pb-8 md:flex-col">
+        <div className="flex flex-col items-center font-bold">
+          <div className="text-nowrap text-center text-3xl uppercase text-accent md:text-3xl lg:text-5xl">
+            Downloads
           </div>
-          <div className="flex min-w-[300px] flex-col gap-4 text-center text-xl md:text-3xl">
-            <a
-              href="/brochure.pdf"
-              target="_blank"
-              className={cn(
-                buttonVariants(),
-                "mt-2 rounded-xl bg-accent px-4 py-3 text-base font-semibold text-white hover:bg-accent/80 md:px-6 md:py-6 md:text-xl",
-              )}
-            >
-              Brochure PDF
-            </a>
-            <a
-              href={frontpage.src}
-              target="_blank"
-              className={cn(
-                buttonVariants(),
-                "mt-2 rounded-xl bg-accent px-4 py-3 text-base font-semibold text-white hover:bg-accent/80 md:px-6 md:py-6 md:text-xl",
-              )}
-            >
-              Brochure front page
-            </a>
-            <a
-              href={backpage.src}
-              target="_blank"
-              className={cn(
-                buttonVariants(),
-                "mt-2 rounded-xl bg-accent px-4 py-3 text-base font-semibold text-white hover:bg-accent/80 md:px-6 md:py-6 md:text-xl",
-              )}
-            >
-              Brochure back page
-            </a>
+        </div>
+      </div>
+      <div className="flex gap-8 max-lg:flex-col">
+        <div className="flex min-w-[300px] flex-col gap-4 text-center text-xl md:text-3xl">
+          <div className="customcol text-nowrap text-center text-xl uppercase text-accent md:text-2xl lg:text-3xl">
+            Manuscripts
           </div>
-        </Section>
-      </main>
-    </div>
+          <a
+            href="/manuscript.pdf"
+            target="_blank"
+            className={cn(
+              buttonVariants(),
+              "mt-2 rounded-xl bg-accent px-4 py-3 text-base font-semibold text-white hover:bg-accent/80 md:px-6 md:py-6 md:text-xl",
+            )}
+          >
+            Template pdf
+          </a>
+          <a
+            href="/manuscript.docx"
+            target="_blank"
+            className={cn(
+              buttonVariants(),
+              "mt-2 rounded-xl bg-accent px-4 py-3 text-base font-semibold text-white hover:bg-accent/80 md:px-6 md:py-6 md:text-xl",
+            )}
+          >
+            Template docx
+          </a>
+        </div>
+        <div className="flex min-w-[300px] flex-col gap-4 text-center text-xl md:text-3xl">
+          <div className="customcol text-nowrap text-center text-xl uppercase text-accent md:text-2xl lg:text-3xl">
+            Abstract (Extended)
+          </div>
+          <a
+            href="/extended_abstract.pdf"
+            target="_blank"
+            className={cn(
+              buttonVariants(),
+              "mt-2 rounded-xl bg-accent px-4 py-3 text-base font-semibold text-white hover:bg-accent/80 md:px-6 md:py-6 md:text-xl",
+            )}
+          >
+            Template pdf
+          </a>
+          <a
+            href="/extended_abstract.docx"
+            target="_blank"
+            className={cn(
+              buttonVariants(),
+              "mt-2 rounded-xl bg-accent px-4 py-3 text-base font-semibold text-white hover:bg-accent/80 md:px-6 md:py-6 md:text-xl",
+            )}
+          >
+            Template docx
+          </a>
+        </div>
+        <div className="flex min-w-[300px] flex-col gap-4 text-center text-xl md:text-3xl">
+          <div className="customcol text-nowrap text-center text-xl uppercase text-accent md:text-2xl lg:text-3xl">
+            Brochure
+          </div>
+          <a
+            href="/brochure.pdf"
+            target="_blank"
+            className={cn(
+              buttonVariants(),
+              "mt-2 rounded-xl bg-accent px-4 py-3 text-base font-semibold text-white hover:bg-accent/80 md:px-6 md:py-6 md:text-xl",
+            )}
+          >
+            Brochure PDF
+          </a>
+          {/* <a
+            href={frontpage.src}
+            target="_blank"
+            className={cn(
+              buttonVariants(),
+              "mt-2 rounded-xl bg-accent px-4 py-3 text-base font-semibold text-white hover:bg-accent/80 md:px-6 md:py-6 md:text-xl",
+            )}
+          >
+            Brochure front page
+          </a>
+          <a
+            href={backpage.src}
+            target="_blank"
+            className={cn(
+              buttonVariants(),
+              "mt-2 rounded-xl bg-accent px-4 py-3 text-base font-semibold text-white hover:bg-accent/80 md:px-6 md:py-6 md:text-xl",
+            )}
+          >
+            Brochure back page
+          </a> */}
+        </div>
+      </div>
+    </main>
   );
 }
