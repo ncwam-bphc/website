8ea73a8bc5f1bf91c50bc7ddbf8ded3c5360e9b8
Shiv Patil <krymzin0311@gmail.com>
2025-03-30 19:35:43 +0530

    chore: update manuscript review reminder mail

--- src/server/actions/manuscript/reminderMail.ts
+++ src/server/actions/manuscript/reminderMail.ts
@@ -17,7 +17,7 @@ const sendMail = async (
 ) => {
   const mailHtml = `<p>Dear ${name},</p>
         
-<p>This is a gentle reminder to review the manuscript assigned to you. Please review and submit the decision on or before 31st January 2025.</p>
+<p>This is a gentle reminder to review the manuscript assigned to you. Please review and submit the decision.</p>
 
 <p><strong>Paper Number:</strong> ${paper}</p>
 
