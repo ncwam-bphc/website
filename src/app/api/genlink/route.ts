923a85443cd689ebc31fd644032ae4d61ee25324
Shiv Patil <krymzin0311@gmail.com>
2025-05-02 19:44:16 +0530

    chore: response

--- src/app/api/genlink/route.ts
+++ src/app/api/genlink/route.ts
@@ -8,13 +8,13 @@ const bodySchema = z.object({
 
 export async function POST(req: NextRequest) {
   const session = await auth();
-  if (!session) return new NextResponse("Unauthorized", { status: 401 });
+  if (!session) return NextResponse.json("Unauthorized", { status: 401 });
   if (session?.user.email !== "f20230740@hyderabad.bits-pilani.ac.in")
-    return new NextResponse("Unauthorized", { status: 401 });
+    return NextResponse.json("Unauthorized", { status: 401 });
   const parsed = bodySchema.safeParse(await req.json());
   if (!parsed.success)
     return NextResponse.json({ error: "Invalid request" }, { status: 400 });
   const { email } = parsed.data;
   const link = await generateSignInLink(email);
-  return new NextResponse(JSON.stringify({ link }), { status: 200 });
+  return NextResponse.json({ link }, { status: 200 });
 }
